3ae7009976e69a270994b1367ccac4accb881093
Nafisaaura25 <nafisaaura25@gmail.com>
2023-09-25 14:40:42 +0700

    Biodata Github Project

---
+++ biodata.py
@@ -0,0 +1,8 @@
+public class App {
+    run|Debug
+    public static void main(String[] args) throws Exception {
+        system.out.println("Hello Naf");
+        system.out.println("Ini adalah program pertama ku");
+    }
+    
+}
